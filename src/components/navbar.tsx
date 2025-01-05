301f60f633fcfaf96906b93b6419b2507e8db5a7
Martin Přívozník <marprivoznik@gmail.com>
2025-01-05 01:00:01 +0100

    Navbar brand

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,6 +5,7 @@ import {
   NavbarMenuToggle,
   NavbarItem,
   NavbarMenuItem,
+  NavbarBrand,
 } from "@nextui-org/navbar";
 import { Link } from "@nextui-org/link";
 import { link as linkStyles } from "@nextui-org/theme";
@@ -23,7 +24,12 @@ export const Navbar = () => {
       position="sticky"
     >
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        <ul className="hidden sm:flex gap-8 justify-start ml-2">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <h1 className="font-bold text-inherit">VŠE Tester V2.0</h1>
+          </NextLink>
+        </NavbarBrand>
+        <ul className="hidden sm:flex gap-5 justify-start ml-2">
           {siteConfig.navItems.map((item) => (
             <NavbarItem key={item.href}>
               <NextLink
